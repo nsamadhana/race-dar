abb6deff4430ba8442499d502d483d208f54fa63
Nevan Samadhana <nsamadhana@gmail.com>
2025-07-17 11:50:15 -0700

    Buttons can now route to their link. Added quiz page

--- src/components/ContentBlock/index.tsx
+++ src/components/ContentBlock/index.tsx
@@ -5,6 +5,7 @@ import { withTranslation } from "react-i18next";
 import { ContentBlockProps } from "./types";
 import { Button } from "../../common/Button";
 import { SvgIcon } from "../../common/SvgIcon";
+import { useHistory } from "react-router-dom";
 import {
   ContentSection,
   Content,
@@ -16,6 +17,107 @@ import {
   ButtonWrapper,
 } from "./styles";
 
+// const ContentBlock = ({
+//   icon,
+//   title,
+//   content,
+//   section,
+//   button,
+//   t,
+//   id,
+//   direction,
+// }: ContentBlockProps) => {
+//   const history = useHistory(); // Add useHistory hook
+
+//   const handleButtonClick = (link: string) => {
+//     history.push(link); // Navigate to the specified link
+//   };
+
+//   const scrollTo = (id: string) => {
+//     const element = document.getElementById(id) as HTMLDivElement;
+//     element.scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   };
+
+//   return (
+//     <ContentSection>
+//       <Fade direction={direction} triggerOnce>
+//         <StyledRow
+//           justify="space-between"
+//           align="middle"
+//           id={id}
+//           direction={direction}
+//         >
+//           <Col lg={11} md={11} sm={12} xs={24}>
+//             <SvgIcon src={icon} width="100%" height="100%" />
+//           </Col>
+//           <Col lg={11} md={11} sm={11} xs={24}>
+//             <ContentWrapper>
+//               <h6>{t(title)}</h6>
+//               <Content>{t(content)}</Content>
+//               {direction === "right" ? (
+//                 <ButtonWrapper>
+//                   {typeof button === "object" &&
+//                     button.map(
+//                       (
+//                         item: {
+//                           color?: string;
+//                           title: string;
+//                         },
+//                         id: number
+//                       ) => {
+//                         return (
+//                           <Button
+//                             key={id}
+//                             color={item.color}
+//                             onClick={() => scrollTo("about")}
+//                           >
+//                             {t(item.title)}
+//                           </Button>
+//                         );
+//                       }
+//                     )}
+//                 </ButtonWrapper>
+//               ) : (
+//                 <ServiceWrapper>
+//                   <Row justify="space-between">
+//                     {typeof section === "object" &&
+//                       section.map(
+//                         (
+//                           item: {
+//                             title: string;
+//                             content: string;
+//                             icon: string;
+//                           },
+//                           id: number
+//                         ) => {
+//                           return (
+//                             <Col key={id} span={11}>
+//                               <SvgIcon
+//                                 src={item.icon}
+//                                 width="60px"
+//                                 height="60px"
+//                               />
+//                               <MinTitle>{t(item.title)}</MinTitle>
+//                               <MinPara>{t(item.content)}</MinPara>
+//                             </Col>
+//                           );
+//                         }
+//                       )}
+//                   </Row>
+//                 </ServiceWrapper>
+//               )}
+//             </ContentWrapper>
+//           </Col>
+//         </StyledRow>
+//       </Fade>
+//     </ContentSection>
+//   );
+// };
+
+// export default withTranslation()(ContentBlock);
+
 const ContentBlock = ({
   icon,
   title,
@@ -26,11 +128,10 @@ const ContentBlock = ({
   id,
   direction,
 }: ContentBlockProps) => {
-  const scrollTo = (id: string) => {
-    const element = document.getElementById(id) as HTMLDivElement;
-    element.scrollIntoView({
-      behavior: "smooth",
-    });
+  const history = useHistory(); // Add useHistory hook
+
+  const handleButtonClick = (link: string) => {
+    history.push(link); // Navigate to the specified link
   };
 
   return (
@@ -52,52 +153,31 @@ const ContentBlock = ({
               {direction === "right" ? (
                 <ButtonWrapper>
                   {typeof button === "object" &&
-                    button.map(
-                      (
-                        item: {
-                          color?: string;
-                          title: string;
-                        },
-                        id: number
-                      ) => {
-                        return (
-                          <Button
-                            key={id}
-                            color={item.color}
-                            onClick={() => scrollTo("about")}
-                          >
-                            {t(item.title)}
-                          </Button>
-                        );
-                      }
-                    )}
+                    button.map((item, id) => (
+                      <Button
+                        key={id}
+                        color={item.color}
+                        onClick={() => handleButtonClick(item.link? item.link : "/")}>
+                      
+                        {t(item.title)}
+                      </Button>
+                    ))}
                 </ButtonWrapper>
               ) : (
                 <ServiceWrapper>
                   <Row justify="space-between">
                     {typeof section === "object" &&
-                      section.map(
-                        (
-                          item: {
-                            title: string;
-                            content: string;
-                            icon: string;
-                          },
-                          id: number
-                        ) => {
-                          return (
-                            <Col key={id} span={11}>
-                              <SvgIcon
-                                src={item.icon}
-                                width="60px"
-                                height="60px"
-                              />
-                              <MinTitle>{t(item.title)}</MinTitle>
-                              <MinPara>{t(item.content)}</MinPara>
-                            </Col>
-                          );
-                        }
-                      )}
+                      section.map((item, id) => (
+                        <Col key={id} span={11}>
+                          <SvgIcon
+                            src={item.icon}
+                            width="60px"
+                            height="60px"
+                          />
+                          <MinTitle>{t(item.title)}</MinTitle>
+                          <MinPara>{t(item.content)}</MinPara>
+                        </Col>
+                      ))}
                   </Row>
                 </ServiceWrapper>
               )}
--- src/router/config.ts
+++ src/router/config.ts
@@ -4,6 +4,11 @@ const routes = [
     exact: true,
     component: "Home",
   },
+  {
+    path: ["/quiz"],
+    exact: true,
+    component: "Quiz",
+  },
 ];
 
 export default routes;
