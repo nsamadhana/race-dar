e397d316842c61a9a5042ddea2b77f013b529e32
Nevan Samadhana <nsamadhana@gmail.com>
2025-07-31 15:42:02 -0700

    add basic quiz layout

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,32 +1,38 @@
 import { lazy } from "react";
 import QuizContent from "../../content/QuizContent.json";
+import QuizBlock from "../../components/QuizBlock";
 import { useLocation } from "react-router-dom";
 
 
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 
-// We need to be using a new type of content block that supports button submission with different content
-const Block = lazy(() => import("../../components/Block"));
+const Block = lazy(() => import("../../components/QuizBlock"));
 
 type LocationState = {
   choices?: string[]; // Define choices as an optional array of strings
 };
-
 const Quiz = () => {
   const location = useLocation<LocationState>();
-  const { choices } = location.state || {}; // Retrieve the passed data
-  console.log("Quiz choices:", choices); // Log the choices for debugging
+  const { choices } = location.state || {};
 
+  const handleSubmit = (selectedChoice: string | null) => {
+    console.log("Selected choice:", selectedChoice);
+  };
+  // TODO: Replace the hardcoded score with dynamic score update 
   return (
-    <Container>
-      <ScrollToTop />
-      <Block
-        title={QuizContent.title}
-        content={QuizContent.text}
-      />
-    </Container>
-    
+    <div>
+      <h1 style={{ textAlign: "center" }}>0/10</h1> 
+      {choices ? (
+        <QuizBlock
+          image="/img/icons/fidel.png" // TODO: Replace this with dyanmic image source
+          choices={choices}
+          onSubmit={handleSubmit}
+        />
+      ) : (
+        <p>No choices available</p>
+      )}
+    </div>
   );
 };
 
